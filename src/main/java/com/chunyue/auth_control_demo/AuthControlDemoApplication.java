2a256c51d35c5559ced7e3d2eec36719b831fe4c
ChunyuePeng <1399068529@qq.com>
2020-07-10 17:41:17 +0800

    initiate

---
+++ src/main/java/com/chunyue/auth_control_demo/AuthControlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chunyue.auth_control_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthControlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthControlDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chunyue/auth_control_demo/AuthControlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunyue.auth_control_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthControlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
